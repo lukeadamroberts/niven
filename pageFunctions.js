ba79eef96fb2722260db1a8ede50069f06777963
Luke Roberts <Luke Roberts>
2016-08-25 11:12:34 +0100

    Added license infomation and readme.

--- pageFunctions.js
+++ pageFunctions.js
@@ -1,4 +1,21 @@
 /**
+* Copyright 2016 Luke Roberts
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
+/**
 * @fileoverview Functions that control the layout of the page
 * @author Luke Roberts
 **/
@@ -75,3 +92,12 @@ function loadFile(){
 		}
 	reader.readAsText(file);
 }
+
+/**
+* showAbout() function
+* Calls an alert that describes the editor and give license
+* infomation.
+**/
+function showAbout(){
+	alert("Niven is a browser based text editor.\n\nUse and modification of this tool are covered by the APACHE 2 license\nviewable at http://www.apache.org/licenses/LICENSE-2.0");
+} 
